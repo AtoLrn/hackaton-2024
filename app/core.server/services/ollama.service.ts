5d0e4f5c270df687502c0bd743382079bbb376f2
Samuel GUENIER <guenier.samuel@gmail.com>
2024-06-25 12:03:56 +0200

    first try with prompt and q&a

--- app/core.server/services/ollama.service.ts
+++ app/core.server/services/ollama.service.ts
@@ -24,15 +24,23 @@ export class OllamaService implements Iollama {
         if (!this.isLoaded) {
             throw new ErrorLoadingTooEarly()
         }
-
-        const message = { role: 'user', content: 'Retourne un tableau json de 10 fruits' }
-        const response = await this.ollama.chat({
+        const question = "CHU d'Angers : Suite à votre opération, si tout va bien, répondez TVB"
+        const response = "tout va bien mais j'ai un peu mal à la tête"
+        const system = {
+            role: 'system',
+            content: 'Tu es un expert de santé qui a plus de vingt ans d\'expérience et qui sait prendre en compte les retours de ses patients afin d\'en tirer des conclusions sur leur état de santé. Je vais te poser des questions et en fonction de la réponse donne moi un indicateur de 0 à 10 sur l\'état de santé du patient.'
+        }
+        const prompt = {
+            role: 'user',
+            content : 'Voici la question posée : "'+question+'", et voici la réponse donnée par le patient : "'+response+'".'
+        }
+        const output = await this.ollama.chat({
             model: 'mistral',
-            messages: [message],
+            messages: [system, prompt],
             format: 'json'
         })
 
-        console.log(response)
+        console.log(output)
 
         return
     }
