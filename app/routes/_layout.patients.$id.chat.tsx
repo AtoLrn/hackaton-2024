b9cfa12a0be60c016f928da93494a287717c78ac
ycenta <thorbenliya@gmail.com>
2024-06-28 14:51:36 +0200

    update button on theme analysis

--- app/routes/_layout.patients.$id.chat.tsx
+++ app/routes/_layout.patients.$id.chat.tsx
@@ -38,8 +38,10 @@ export default function Index() {
     addSubscription: (id: string, cb: () => void) =>void
   }>()
   const fetcher = useFetcher<typeof action>()
+  const fetcherTheme = useFetcher<typeof action>()
   const { patient } = useLoaderData<typeof loader>()
   const [ loading, setLoading ] = useState(false)
+  const [ themeLoading, setThemeLoading ] = useState(false)
 
 
   useEffect(() => {
@@ -49,14 +51,21 @@ export default function Index() {
     }
   }, [fetcher.data])
 
+  useEffect(() => {
+    if (fetcherTheme.data) {
+      setThemeLoading(true)
+      subscriptions.addSubscription(fetcherTheme.data.id, () => setThemeLoading(false))
+    }
+  })
+
 
   return <div className="flex-1 p-12 flex items-stretch justify-stretch gap-12">
       <div className="flex-1 bg-[#f9faff] shadow-lg rounded-md p-8 flex flex-col gap-4">
         <div className="flex items-center justify-between">
           <h1 className="tracking-wider text-xl font-bold">Chat</h1>
-          <fetcher.Form method="POST" action={`/patients/${patient.id}/thematize`}>
-            <button className="px-4 py-2 bg-blue-500 text-white rounded-md shadow-md" type="submit">Analyser</button>
-          </fetcher.Form>
+          <fetcherTheme.Form method="POST" action={`/patients/${patient.id}/thematize`}>
+            <button className="px-4 py-2 bg-blue-500 text-white rounded-md shadow-md" disabled={fetcherTheme.state === 'submitting' || themeLoading}>{fetcherTheme.state !== 'submitting' && !themeLoading ? 'Analyze Thematize' : 'Thematizing...'}</button>
+          </fetcherTheme.Form>
           <fetcher.Form method="POST" action={`/patients/${patient.id}/anonymize`}>
             <button className="px-4 py-2 bg-[#fb4f14] text-white rounded-md shadow-md" disabled={fetcher.state === 'submitting' || loading}>{fetcher.state !== 'submitting' && !loading ? 'Export' : 'Exporting...'}</button>
           </fetcher.Form>
@@ -69,7 +78,7 @@ export default function Index() {
                 return (
                   <li className={`p-2 rounded-lg shadow-xl max-w-[80%] ${fromUser ? "self-end" : "text-black bg-[#ff8b64]"}`} key={content}>
                     <p>{content}</p>
-                    <p className="bg-blue-500 text-white px-2 py-1 rounded-md w-50">{theme}</p>
+                    {theme && <p className="bg-blue-500 text-white px-2 py-1 rounded-md w-50">{theme}</p>}
                   </li>
                 );
                     })
